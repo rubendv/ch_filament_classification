615fb507075c73d87dafaad93193c60f3584b1b2
Ruben De Visscher <ruben.devisscher@observatory.be>
2015-03-25 15:23:16 +0100

    Removed test.py

--- src/test.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt
-import numpy as np
-
-x = np.linspace(-np.pi, np.pi, 1000)
-plt.plot(x, np.cos(x), label="cosine")
-plt.plot(x, np.sin(x), label="sine")
-plt.legend(loc="best")
-plt.show()
